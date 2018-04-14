01ff8a84ea1f037c5285df67caa4e1b942d05412
zhammer <zachary_hammer@brown.edu>
2018-04-14 09:15:50 -0400

    Setup architecture directory structure

---
+++ finnegan_forever/delivery/aws/read_finnegan.py
@@ -0,0 +1,6 @@
+"""AWS lambda function delivery for reading the current passage of Finnegan's Wake."""
+
+
+def handler(event, context):
+    """AWS lambda event handler for read_finnegan."""
+    raise NotImplementedError
