9f6c067d56d98f1e8026153a41a74d4fed5a8359
zhammer <zachary_hammer@brown.edu>
2018-04-15 12:32:23 -0400

    Add read_current_passage use case.
    Move text fixtures into global conftest.py file.
    
    Remove TimeGateway and Context struct. (`seconds` are passed as an
    integer to read_current_passage directly. `scroll` is passed directly
    rather than through a `context` struct.)

---
+++ tests/integration/test_read_current_passage.py
@@ -0,0 +1,51 @@
+"""Integration test module for the read_current_passage use case."""
+
+import pytest
+from finnegan_forever.read_current_passage import read_current_passage
+
+
+@pytest.mark.parametrize('text', [
+    pytest.lazy_fixture('a_field_of_cotton'),
+    pytest.lazy_fixture('i_hear_an_army')
+])
+class TestReadCurrentPassage:
+    """Test suite for read_current_passage."""
+
+    @pytest.mark.parametrize('size', [
+        10,
+        20,
+        30,
+        1000
+    ])
+    def test_passage_size(self, text, size):
+        # Given
+        scroll = InMemoryScrollGateway(text)
+        passage_size = size
+        window_length = 4
+        seconds = 0
+
+        # When
+        passage = read_current_passage(scroll, passage_size, window_length, seconds)
+
+        # Then
+        expected = text[0:passage_size]
+        assert passage == expected
+
+# Mocks
+
+class InMemoryScrollGateway:
+    """Mock scroll gateway class. Uses an in-memory text block rather """
+
+    def __init__(self, text):
+        """C'tor"""
+        self.text = text
+
+
+    def __len__(self):
+        """Get the length of self.text."""
+        return len(self.text)
+
+
+    def read_passage(self, offset, passage_size):
+        """Read a passage of `passage_size` bytes starting at `offset`."""
+        return self.text[offset: offset + passage_size]
--- tests/unit/test_scroll_gateway.py
+++ tests/unit/test_scroll_gateway.py
@@ -48,29 +48,3 @@ class TestScrollGateway:
                 assert scroll_passage.encode('utf-8') == expected
 
                 offset += block_size
-
-
-@pytest.fixture()
-def a_field_of_cotton():
-    """Data fixture for Matsuo Basho's haiku 'A Field of Cotton'."""
-    return b"""A field of cotton--
-as if the moon
-  had blossomed."""
-
-@pytest.fixture()
-def i_hear_an_army():
-    """Data fixture for Joyce's poem 'I Hear an Army'."""
-    return b"""I hear an army charging upon the land,
-  And the thunder of horses plunging, foam about their knees:
-Arrogant, in black armour, behind them stand,
-  Disdaining the reins, with fluttering whips, the charioteers.
-
-They cry unto the night their battle-name:
-  I moan in sleep when I hear afar their whirling laughter.
-They cleave the gloom of dreams, a blinding flame,
-  Clanging, clanging upon the heart as upon an anvil.
-
-They come shaking in triumph their long, green hair:
-  They come out of the sea and run shouting by the shore.
-My heart, have you no wisdom thus to despair?
-  My love, my love, my love, why have you left me alone?"""
