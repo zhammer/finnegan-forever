b28a98a41ff8ebe6db737b3775d1cec6325856d5
zhammer <zachary_hammer@brown.edu>
2018-04-23 22:34:28 -0400

    Add lambda proxy cors headers.

--- finnegan_forever/delivery/aws/read_finnegan.py
+++ finnegan_forever/delivery/aws/read_finnegan.py
@@ -36,5 +36,9 @@ def handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True
+        },
         'body': json.dumps(body)
     }
