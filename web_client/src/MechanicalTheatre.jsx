558fe89c18d890776ab64abcea4ed68c5bdf7411
zhammer <zachary_hammer@brown.edu>
2018-05-01 00:13:18 -0400

    Convert MechanicalTheatre to a functional component

--- web_client/src/MechanicalTheatre.jsx
+++ web_client/src/MechanicalTheatre.jsx
@@ -4,28 +4,23 @@ import cloudRight from './assets/cloud-right.png';
 import waves from './assets/waves.png';
 import './MechanicalTheatre.css';
 
-class MechanicalTheatre extends Component {
-    render() {
-        return (
-            <div className="stage">
-              <div className="clouds">
-                <div className="cloudRight">
-                  <img className="picture" src={cloudRight} alt=""/>
-                </div>
-                <div className="cloudLeft">
-                  <img className="picture" src={cloudLeft} alt=""/>
-                </div>
-
-              </div>
-              <div className="content">
-                {this.props.children}
-              </div>
-              <div className="waves">
-                <img className="picture" src={waves} alt=""/>
-              </div>
-            </div>
-        );
-    }
-}
+const MechanicalTheatre = ({ children }) => (
+    <div className="stage">
+      <div className="clouds">
+        <div className="cloudRight">
+          <img className="picture" src={cloudRight} alt=""/>
+        </div>
+        <div className="cloudLeft">
+          <img className="picture" src={cloudLeft} alt=""/>
+        </div>
+      </div>
+      <div className="content">
+        {children}
+      </div>
+      <div className="waves">
+        <img className="picture" src={waves} alt=""/>
+      </div>
+    </div>
+);
 
 export default MechanicalTheatre;
